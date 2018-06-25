9f4f513b65493baf690bf58a96cde12f7b5901cf
ckdgus2938 <ckdgus2938@gmail.com>
2018-06-25 10:38:56 +0900

    2018_06_25

--- project/controller/printer.py
+++ project/controller/printer.py
@@ -2,23 +2,26 @@ import os
 from werkzeug.utils import secure_filename
 from project import app
 from project.models.calculate_Similarity import calculate_Similarity, SIFT_detector
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect,Response
 
 
 # filename Initialize
 global filename
-filename = "images/9.bmp"
+global result
+filename = "images/Initialization.jpg"
+result = "Please Input Image"
 tempImages = []
 tempPercent = []
 tests= []
 
 
+
 # Index
 @app.route('/', methods=['GET'])
 def start():
-
+    global result
     return render_template('index.html',
-                           filename=filename, tempImages = tempImages)
+                           filename=filename, result= result, tempImages = tempImages)
 
 
 def allowed_file(filename):
@@ -48,6 +51,7 @@ def upload_file():
     global filename
     global tempImages
     global tempPercent
+    global result
     #global tests
     if request.method == 'POST':
         print( request.files)
@@ -68,20 +72,20 @@ def upload_file():
             filename = "images/" + secure_filename(file.filename)
             img = os.path.join(app.config['UPLOAD_FOLDER']  , filename)
             file.save(img)
-
+            tempImages = []
             # models.calculate_Similarity
             for root, dirs, files in os.walk('project/static/images/'):
                 for fname in files:
                     try:
                         full_fname = os.path.join(root, fname)
                         #full_fname = "project/static/images/" + fname
+
                         fileType = full_fname.split('.')[1]
-                        if (fileType == "bmp"):
+                        if (fileType == "bmp" and img != full_fname):
                             SIFT_detector(img, full_fname, fname)
                             diff = calculate_Similarity(img, full_fname)
-                            #tempPercent.append( diff)
-                            #tempImages.append(  "diff_images/diff_" + fname)
-                            tempImages.append( {'diff':diff, 'path': "diff_images/diff_" + fname} )
+
+                            tempImages.append( {'diff':diff, 'path': "diff_images/diff_" + fname } )
 
                     except:
                         print("")
@@ -89,8 +93,11 @@ def upload_file():
             tempImages= sorted( tempImages, key = lambda k : k['diff'])
             # if 처음 등장 "new"
             # 이미 등장했던 사람 "already"
+            if( tempImages[0]['diff']   > 0.7):
+                result = "New"
+            else:
+                result = "Already"
+            return render_template('return.html',
+                                   oing=result)
 
-            return "new"
-
-        print("error")
         return render_template('error.html')
